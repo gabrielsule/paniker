9585920b72cc1329c3cadc5daf99813a1c53c148
Gabriel Sule <gabrielsule@gmail.com>
2023-10-04 03:30:51 -0300

    comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express(); // Initializing Express App
 
 // Sending Hello World when anyone browse your webpage
+//
 
 app.get("/*", (req, res) => {
     res.send('Hello World');
