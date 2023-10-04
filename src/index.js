8bd4465a3a66ac31c24adce4563d1afea1f68ff7
Gabriel Sule <gabrielsule@gmail.com>
2023-10-04 09:10:27 -0300

    qqq

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express(); // Initializing Express App
 
+// Sending Hello World when anyone browse your webpage
+
 app.get("/*", (req, res) => {
     res.send('Hello World');
 });
---
+++ src/services/paniker.js
@@ -0,0 +1,57 @@
+const { pool } = require('../db/citus');
+
+const getPaniker = async (email) => {
+  const qry = `
+    select * from paniker
+    where email = '${email}';
+  `;
+
+  const res = await pool.query(qry)
+  if (!res?.rows[0]) {
+    return [];
+  }
+  else {
+    return res.rows[0];
+  }
+};
+
+const postPaniker = async (body) => {
+  const qry = `
+    insert into paniker
+    (nickname, phone, email, coords)
+    values
+    ('${body.nickname}', ${body.phone}, '${body.email}', point(${body.coords.latitude},${body.coords.longitude}))
+    returning id
+  `;
+
+  const res = await pool.query(qry)
+  if (!res?.rows[0]) {
+    return [];
+  }
+  else {
+    return res.rows[0];
+  }
+};
+
+
+const putPaniker = async (body) => {
+  const qry = `
+    update paniker
+    set nickname = '${body.nickname}', phone = ${body.phone}, coords = point(${body.coords.latitude},${body.coords.longitude})
+    where email = '${body.email}'
+  `;
+
+  const res = await pool.query(qry)
+  if (!res?.rows[0]) {
+    return [];
+  }
+  else {
+    return res.rows[0];
+  }
+};
+
+module.exports = {
+  getPaniker: getPaniker,
+  postPaniker: postPaniker,
+  putPaniker: putPaniker
+}
